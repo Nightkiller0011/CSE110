b6dc878051e6ef9d9c7c2d11c5d03eb86066cf61
Nightkiller0011 <joshuaellis152@gmail.com>
2022-05-25 10:02:43 -0600

    work

--- assignment7and8.py
+++ assignment7and8.py
@@ -2,7 +2,25 @@
 File: assignment7and8.py
 Author: Joshua Ellis
 
-Purpose: 
+Purpose: Create and Guess a secret word.
 
 # Above and Beyond completed.
 """
+import random
+playagain = 'yes'
+while playagain.lower() == "yes":
+    print(f"\n  /------------\\ \nGenerating new word.\n  \\------------/ \n")
+    go_again = 1
+    guesses = 0
+    hint = ""
+    word = ""
+    print(f"Your hint is: {hint}\n")
+    while go_again == 1:
+        guess = input(f"What is your guess? ")
+        if guess.lower() == word:
+            go_again = 0
+            print(f"Good job! you guessed it! ")
+        else:
+            print(f"Your hint is: {hint}\n")
+        go_again = int(input(f"Test Go_Again: "))
+    playagain = input(f"Would you like to play again? ")
